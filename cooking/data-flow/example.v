c2322f019bb25dc0fae68dc377017916a6d589ec
Idhibhat Pankam <marginpankam@gmail.com>
2024-08-10 12:29:45 +0700

    gig

--- cooking/data-flow/example.v
+++ cooking/data-flow/example.v
@@ -1,10 +1,6 @@
-module example(a, b, c, d, e, f, Y);
-input a, b, c, d, e, f;
-output Y;
-wire t1, t2, t3;
-
-nand G1(t1, a, b);
-and  G2(t2, c, ~b, d);
-nor  G3(t3, e, f);
-nand G4(Y, t1, t2, t3);
+module example(input a, b, c, d, e, f, output Y, wire t1, t2, t3);
+assign t1 = a & b;
+assign t2 = c & ~b & d;
+assign t3 = ~(e | f);
+assign Y = ~(t1 & t2 & t3);
 endmodule
