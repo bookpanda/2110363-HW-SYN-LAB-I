5f94f67b7488655e2843971c6931cec56ff5622c
Idhibhat Pankam <marginpankam@gmail.com>
2024-10-14 18:17:35 +0700

    init

---
+++ lab05/lab05.srcs/sources_1/new/control.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/14/2024 06:04:08 PM
+// Design Name: 
+// Module Name: control
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module control(
+
+    );
+endmodule
